8363b083c32b93004d76f58fc13983ebff9601a7
Bikal <bikaladhikari5@gmail.com>
2024-10-02 18:47:29 +1000

    chnaging element behavior using attribute directive

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -3,9 +3,20 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('Directive created');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+    if (wantsToLeave) {
+      return;
+    }
+    event.preventDefault();
   }
 }
